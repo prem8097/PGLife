4cd4716ac02d3c631fe850dd5f8a5aaa8d512349
prem <105004386+prem8097@users.noreply.github.com>
2022-10-09 23:05:53 +0530

    Update PGLifehome.php

--- PGLifehome.php
+++ PGLifehome.php
@@ -4,6 +4,13 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" rel="stylesheet">
         <script src="js/bootstrap.bundle.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <link  href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->  
         <link href="css/pglife.css" rel="stylesheet">
         <title>Home | PGLife</title>
     </head>
